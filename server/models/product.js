e3be7c07d604048e7d673f8494db960e97ee547d
Paribesh Nepal <nepalparibesh01@gmail.com>
2024-02-09 22:08:21 +0545

    modified the models

--- server/models/product.js
+++ server/models/product.js
@@ -6,6 +6,7 @@ const productSchema = new Schema({
   name: { type: String, required: true },
   price: { type: Number, required: true },
   description: { type: String, required: true },
+  userId: mongoose.Schema.Types.ObjectId,
 });
 
 const Product = mongoose.model("product", productSchema);
--- server/models/user.js
+++ server/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  products: [{ type: mongoose.Schema.Types.ObjectId }],
 });
 
 const User = mongoose.model("user", userSchema);
